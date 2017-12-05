2b73a63021729848d1c8d13b294b420f01e541d5
SMR <SmugglerSMR@gmail.com>
2017-12-06 09:57:20 +1000

    Initialization. First Build

---
+++ app/src/main/java/com/deeepsense/smuggler/nfcp2p_demo_15/MainActivity.java
@@ -0,0 +1,13 @@
+package com.deeepsense.smuggler.nfcp2p_demo_15;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
